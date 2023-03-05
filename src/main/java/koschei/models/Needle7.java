0ee16245ce552a73157621cb44917b52ffe95565
79144 <tixlag@yandex.ru>
2023-03-06 01:24:11 +0800

    Задание выполнено

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,12 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    @Autowired
+    Deth8 deth;
 
     @Override
     public String toString() {
